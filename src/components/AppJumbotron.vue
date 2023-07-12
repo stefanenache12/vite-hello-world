8da3e65357c046905cfe46a033c7adcbe7031c19
stefanenache12 <stefanenache12@gmail.com>
2023-07-12 15:03:13 +0300

    END PROJECT

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  data() {
+
+  },
+}
+
+</script>
+
+<template>
+    <img src="https://img.freepik.com/free-photo/galaxy-nature-aesthetic-background-starry-sky-mountain-remixed-media_53876-126761.jpg?w=996&t=st=1689162890~exp=1689163490~hmac=c9860f53e777df76c653cd3d32258e54fdcfa78765259a88f00fdcca16ed7a2d" alt="">
+</template>
+
+<style scoped>
+img {
+    max-width: 80%;
+    margin-top: 100px;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+
+export default {
+  data() {
+    return { 
+      title: 'La mia prima app con Vite!'
+    }
+  },
+}
+
+</script>
+
+<template>
+
+<h1>
+    {{ title }}
+</h1>
+
+
+</template>
+
+<style scoped>
+h1 {
+    background-color: azure;
+    color: black;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 25px;
+}
+</style>
